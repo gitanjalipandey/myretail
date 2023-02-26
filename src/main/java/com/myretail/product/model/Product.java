84475b4ec1d42179b6704722a0a09d64cf26fcd6
gitanjalipandey <gitanjali2004@gmail.com>
2023-02-26 10:29:50 -0800

    Create Product.java

---
+++ src/main/java/com/myretail/product/model/Product.java
@@ -0,0 +1,37 @@
+package com.myretail.product.model;
+
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table("product")
+public class Product {
+    @PrimaryKey("product_id")
+    String id;
+
+    @Column("name")
+    String name;
+
+    public Product(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
