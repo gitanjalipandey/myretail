35269b836df7102c1de3d06ea33a85dc395aec76
gitanjalipandey <gitanjali2004@gmail.com>
2023-02-26 10:29:18 -0800

    Create ProductRepository.java

---
+++ src/main/java/com/myretail/product/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+import com.myretail.product.model.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, String> {
+}
